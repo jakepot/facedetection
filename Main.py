eb541782c15641fbeaf80c270a7bf0eb97df3a0c
Patryk Stasiewski <patryk_stasiewski@wp.pl>
2017-11-20 00:33:33 +0100

    Slaba wersja wykrywania twarzy - przez ruch

--- Main.py
+++ Main.py
@@ -16,17 +16,33 @@ gl = cv2.imread("glasses.png", -1)
 faceCascade = cv2.CascadeClassifier(facePath)
 eyesCascade = cv2.CascadeClassifier(eyesPath)
 
+#czy pokazywac prostokaty wokol wykrytej twarzy
 showRectangles=1
 
+#tryb pracy - ktory algorytm ma byc uzywany
 programMode = 1
 
+#parametry redukcji drzenia okularow
 noiseReactionDelay = 10
 noiseTolerantion = 10
 oldGlassesPosX = 0
 oldGlassesPosY = 0
 oldGlassesLiveTime = 0
 
+#parametry wykrywania poprzez ruch
+possibleColorDiff = 10
+boundaryFound = -1
+boundaryX = -1
+boundaryY = -1
+boundaryW = -1
+boundaryH = -1
+
+positionChangeParam = 0.85
+scaleChangeParam = 0.97
+
 #video_capture.set()
+ret, oldFrame = video_capture.read()
+oldGray = cv2.cvtColor(oldFrame, cv2.COLOR_BGR2GRAY);
 
 # Read the image
 while True:
@@ -85,8 +101,34 @@ while True:
 
     if programMode == 2:
         ret, frame = video_capture.read()
+        frame = cv2.flip(frame, 1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        foundObject = cv2.absdiff(oldGray, gray)
+        thresh = cv2.threshold(foundObject, possibleColorDiff, 255, cv2.THRESH_BINARY)[1]
+        kernel = np.ones((5, 5), np.uint8)
+        thresh = cv2.erode(thresh, kernel, iterations=2)
+        thresh = cv2.dilate(thresh, kernel, iterations=35)
+        thresh = cv2.erode(thresh, kernel, iterations=20)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for c in contours:
+            (x, y, w, h) = cv2.boundingRect(c)
+            if w / h > 1.5 or h / w > 1.5:
+                continue
+            if boundaryFound == -1:
+                boundaryX = x
+                boundaryY = y
+                boundaryW = w
+                boundaryH = h
+                boundaryFound = 0
+            else:
+                boundaryX = np.int32(boundaryX * positionChangeParam + x * (1 - positionChangeParam))
+                boundaryY = np.int32(boundaryY * positionChangeParam + y * (1 - positionChangeParam))
+                boundaryW = np.int32(boundaryW * scaleChangeParam + w * (1 - scaleChangeParam))
+                boundaryH = np.int32(boundaryH * scaleChangeParam + h * (1 - scaleChangeParam))
+
+        cv2.rectangle(frame, (boundaryX, boundaryY), (boundaryX + boundaryW, boundaryY + boundaryH), (0, 255, 0), 2)
         cv2.imshow("Faces", frame)
+        oldGray = gray
 
 
     key = cv2.waitKey(10)
