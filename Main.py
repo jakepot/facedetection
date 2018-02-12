d552afe758a85c21f93bc44d6a7d94a2896cb01e
kotlecik666 <jakub.pyra.1@gmail.com>
2018-02-12 13:54:18 +0100

    od teraz to jest projekt na SW

--- Main.py
+++ Main.py
@@ -1,35 +1,30 @@
 import numpy as np
 import cv2
 import sys
+import datetime
 
 # Get user supplied values
-#imagePath = "drdoktor.jpg"
-facePath = "haarcascade_frontalface_default.xml"
-eyesPath = "haarcascade_eye.xml"
 video_capture = cv2.VideoCapture(0)
-gl = cv2.imread("glasses.png", -1)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+filename = str(datetime.datetime.now()).replace(" ", "_").replace(":", "_")[:-7] + ".avi"
+out = cv2.VideoWriter(filename, fourcc, 25.0, (640, 480))
 
-#video_capture.set(3, 640)
-#video_capture.set(4, 480)
+recording = False
+movement_detected = False
+noMoveDelay = datetime.timedelta(0, 3)
+last_movement = datetime.datetime.now()
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(facePath)
-eyesCascade = cv2.CascadeClassifier(eyesPath)
+# tryb pracy - ktory algorytm ma byc uzywany
+programMode = 2
 
-#czy pokazywac prostokaty wokol wykrytej twarzy
-showRectangles=1
-
-#tryb pracy - ktory algorytm ma byc uzywany
-programMode = 1
-
-#parametry redukcji drzenia okularow
+# parametry redukcji drzenia okularow
 noiseReactionDelay = 10
 noiseTolerantion = 10
 oldGlassesPosX = 0
 oldGlassesPosY = 0
 oldGlassesLiveTime = 0
 
-#parametry wykrywania poprzez ruch
+# parametry wykrywania poprzez ruch
 possibleColorDiff = 10
 boundaryFound = -1
 boundaryX = -1
@@ -40,65 +35,11 @@ boundaryH = -1
 positionChangeParam = 0.85
 scaleChangeParam = 0.97
 
-#video_capture.set()
 ret, oldFrame = video_capture.read()
-oldGray = cv2.cvtColor(oldFrame, cv2.COLOR_BGR2GRAY);
+oldGray = cv2.cvtColor(oldFrame, cv2.COLOR_BGR2GRAY)
 
 # Read the image
 while True:
-    if programMode == 1:
-        ret, frame = video_capture.read()
-        frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        glasses = gl
-
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.08,
-            minNeighbors=4,
-            minSize=(30, 30)
-        )
-
-        for (x, y, w, h) in faces:
-            if showRectangles:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            #region of interest
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            eyes = eyesCascade.detectMultiScale(roi_gray, scaleFactor=1.05, minNeighbors=8, minSize=(3,3))
-            sglasses = cv2.resize(glasses, (w, w/3))
-            leftEye = 1280
-            g = 1000
-            for (ex, ey, ew, eh) in eyes:
-                if showRectangles:
-                    cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0, 255, 0), 2)
-                if ex < leftEye:
-                    leftEye = ex
-                    g = ey
-            if leftEye < 1280:
-                if abs(x - oldGlassesPosX) < noiseTolerantion and abs(g - oldGlassesPosY) < noiseTolerantion and oldGlassesLiveTime < noiseReactionDelay:
-                    x = oldGlassesPosX
-                    g = oldGlassesPosY
-                    oldGlassesLiveTime = oldGlassesLiveTime + 1
-                else:
-                    oldGlassesLiveTime = 0
-                    oldGlassesPosX = x
-                    oldGlassesPosY = g
-
-                y1, y2 = g, g + sglasses.shape[0]
-                x1, x2 = x, x + sglasses.shape[1]
-
-                alpha_s = sglasses[:,:,3] / 255.0
-                alpha_l = 1.0 - alpha_s
-                for c in range(0, 3):
-                    frame[y+y1:y+y2, x1:x2, c] = (alpha_s * sglasses[:,:, c] +
-                                                  alpha_l * frame[y+y1:y+y2, x1:x2, c])
-
-
-        cv2.imshow("Faces", frame)
-
     if programMode == 2:
         ret, frame = video_capture.read()
         frame = cv2.flip(frame, 1)
@@ -127,19 +68,30 @@ while True:
                 boundaryH = np.int32(boundaryH * scaleChangeParam + h * (1 - scaleChangeParam))
 
         cv2.rectangle(frame, (boundaryX, boundaryY), (boundaryX + boundaryW, boundaryY + boundaryH), (0, 255, 0), 2)
+        # saving video
+        if movement_detected:
+            last_movement = datetime.datetime.now()
+            if not recording:
+                filename = str(datetime.datetime.now()).replace(" ", "_").replace(":", "_")[:-7] + ".avi"
+                out = cv2.VideoWriter(filename, fourcc, 25.0, (640, 480))
+                recording = True
+            out.write(frame)
+        else:
+            if recording:
+                if datetime.datetime.now() - last_movement < noMoveDelay:  # dokoncz
+                    out.write(frame)
+                else:
+                    recording = False
+                    out.release()
+
+        # out.write(frame)
         cv2.imshow("Faces", frame)
         oldGray = gray
 
-
     key = cv2.waitKey(10)
-    if key == 115: # s
-        showRectangles = 1 - showRectangles
-    elif key == 27: # ESC
+    if key == 27:  # ESC
         break
-    elif key == 49: # 1
-        programMode = 1
-    elif key == 50: # 2
-        programMode = 2
 
 video_capture.release()
+out.release()
 cv2.destroyAllWindows()
